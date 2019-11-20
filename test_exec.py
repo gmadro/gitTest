2ffefdc5efff0349fc7ea43266dad4440b9c12ac
Greg Madro <greg.madro@gmail.com>
2019-11-20 15:34:07 -0500

    Update test_exec.py

--- test_exec.py
+++ test_exec.py
@@ -1,11 +1,17 @@
-import unittest
-import basecode
+import os
+import sys
 
-class Testbasecase(unittest.TestCase):
-    def test_calc(self):
-        self.assertEqual(basecode.calc(3,4), 7)
+def run_test(project):
+    CWD = os.getcwd()
+    os.system('python ' + CWD + '\\' + project + '\\test.py')
 
 def main():
-    unittest.main()
+
+    scripthelp = 'Must provide product dir: \n test_exec.py <PRODUCT DIR>'
+
+    if len(sys.argv) == 1:
+        print(scripthelp)
+    else:
+        run_test(sys.argv[1])
 
 main()
